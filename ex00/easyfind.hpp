0cfb925afc314967be4b91048c521f0a09926d2e
17931837B <tomoya.bb1014@gmail.com>
2025-05-11 14:22:25 +0900

    fix ex00

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -3,20 +3,23 @@
 
 #include <iostream>
 #include <iterator>
+#include <algorithm>
 
 template <typename T>
 typename T::iterator easyfind(T& con, int value)
 {
-	typename T::iterator it = con.begin();
-	typename T::iterator ite = con.end();
+	// typename T::iterator it = con.begin();
+	// typename T::iterator ite = con.end();
 
-	while (it != ite)
-	{
-		if (*it == value)
-			return it;
-		it++;
-	}
-	return (ite);
+	// while (it != ite)
+	// {
+	// 	if (*it == value)
+	// 		return it;
+	// 	it++;
+	// }
+	// return (ite);
+	typename T::iterator it = std::find(con.begin(), con.end(), value);
+	return (it);
 }
 
 #endif
